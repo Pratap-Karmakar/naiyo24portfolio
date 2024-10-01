f31dcb3f31809298bfae7f1f0f32c129b2412d9d
Pratap Karmakar <pratap2501.pk@gmail.com>
2024-10-01 16:09:28 +0530

    spell check

--- src/app/viewprice/page.tsx
+++ src/app/viewprice/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-7xl text-[#FAF7F0] flex items-center justify-center font-bold pt-24'>Comming Soon!</div>
+    <div className='text-7xl text-[#FAF7F0] bg-black flex items-center justify-center font-bold pt-24'>Coming Soon!</div>
   )
 }
 
